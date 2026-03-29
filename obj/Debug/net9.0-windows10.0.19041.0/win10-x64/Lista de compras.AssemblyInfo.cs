b6a734393f4d0c73fad2507c975d7c7818c423a8
Julio070708 <juliocesarsouzamartines@gmail.com>
2026-03-29 18:26:45 -0300

    Adicionando PDF do projeto

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Lista de compras.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Lista de compras.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista de compras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+41d6228b86ba5d324f074945a260cb2a44f55127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c5e4b4273960f67c7ee4a385368bf654dd5efd91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista de compras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista de compras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
