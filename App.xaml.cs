c5e4b4273960f67c7ee4a385368bf654dd5efd91
Julio070708 <juliocesarsouzamartines@gmail.com>
2026-03-29 18:13:38 -0300

    Primeiro commit - projeto lista de compras

---
+++ App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Lista_de_compras.Helper;
+
+namespace Lista_de_compras
+{
+    public partial class App : Application
+    {
+        static SQLiteDatabaseHelpers? _db;
+
+        public static SQLiteDatabaseHelpers Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelpers(path);
+                }
+
+                return _db;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace Lista_de_compras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; } = "";
+
+        public double Quantidade { get; set; }
+
+        public double Preco { get; set; }
+
+        public double Total => Quantidade * Preco;
+    }
+}
